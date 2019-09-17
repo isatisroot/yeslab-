4a3ff00eb487b5250862639003a7cf4fdb163cb5
Hanson <hairunxu@gmail.com>
2019-09-17 18:10:49 +0800

    完善用户中心功能模块，新增实验地址模型类对象

--- backend/experiments/models.py
+++ backend/experiments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class LabAdress(models.Model):
+    """
+    创建记录rock和lab的ip的自关联表
+    """
+    name = models.CharField(max_length=20,verbose_name='Rock或Lab名')
+    adress = models.CharField(max_length=60,verbose_name='实验接口')
+    rockName = models.ForeignKey('self',null=True,blank=True,verbose_name='从属哪个Rock',on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = 'tb_labadress'
+        verbose_name = '实验地址表'
+        verbose_name_plural = verbose_name
